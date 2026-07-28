7dcf2f8a7cb301b8ed3b2b05bf82cbbc8a2360ff
Nimesh kewat <nimeshgamer37@gmail.com>
2026-07-28 22:57:30 +0530

    feat: write multiple rate limiters

---
+++ server/src/middlewares/rateLimiters.ts
@@ -0,0 +1,56 @@
+import { rateLimit } from "express-rate-limit";
+
+export const registerLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many register attempts. Please try again later.",
+  },
+});
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many login attempts. Please try again later.",
+  },
+});
+
+export const forgotPasswordLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 3,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many forgot password attempts. Please try again later.",
+  },
+});
+
+export const checkoutLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many checkout attempts. Please try again later.",
+  },
+});
+
+export const uploadImageLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 20,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many upload image attempts. Please try again later.",
+  },
+});
